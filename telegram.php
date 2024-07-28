c94593e4e5c3a1056379973be39e4be22e8ca11b
Алексей Артемович <81881832+lesha2701@users.noreply.github.com>
2024-07-28 12:02:21 +0300

    123

--- telegram.php
+++ telegram.php
@@ -1,19 +1,25 @@
 <?php
 
 $name = $_POST['name'];
-$phone = $_POST['phone'];
+$phone = $_POST['tel'];
 $email = $_POST['email'];
+$message = $_POST['message'];
+$delivery_option = $_POST['delivery_option']; // Получаем значение из селекта
 $token = "7429418355:AAEhCXGcDivq6AxSPqSUOiLDoompdCjdj3k";
 $chat_id = "-1002243481550";
+
 $arr = array(
   'Имя пользователя: ' => $name,
   'Телефон: ' => $phone,
-  'Email' => $email
+  'Email: ' => $email,
+  'Сообщение: ' => $message,
+  'Вариант доставки: ' => $delivery_option // Добавили вариант доставки
 );
 
+$txt = ""; // Объявляем переменную $txt
 foreach($arr as $key => $value) {
   $txt .= "<b>".$key."</b> ".$value."%0A";
-};
+}
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
